3f250468f2ed3c0f4b2a32e60d1aac10136891b3
matt maddex <mmaddex@render.com>
2022-10-31 16:15:43 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "starting on 4000"
-bundle exec puma -p 4000 &
-echo "starting on 5000"
-bundle exec puma -p 5000 &
+echo "starting on all the ports"
+bundle exec puma -p 4000 & bundle exec puma -p 5000
 echo "done started"
