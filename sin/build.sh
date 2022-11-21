5034628bd4957830c7378107ba3909ffbf563b20
matt maddex <mmaddex@render.com>
2022-11-21 11:18:53 -0800

    Update build.sh

--- sin/build.sh
+++ sin/build.sh
@@ -14,4 +14,14 @@ echo $PATH
 casperjs --version
 phantomjs --version
 
+echo "setting in buuild"
+
+export OPENSSL_CONF=/etc/ssl
+# Just need to remove the /sin from the path and this should work for anyone
+export PATH="$PATH:/opt/render/project/src/sin/node_modules/phantomjs-prebuilt/lib/phantom/bin/"
+export PATH="$PATH:/opt/render/project/src/sin/node_modules/casperjs/bin/"
+
+casperjs --version
+phantomjs --version
+
 bundle install
