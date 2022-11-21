1acec8bee6b22aec26d708baf42257607f2f35a8
matt maddex <mmaddex@render.com>
2022-11-21 10:59:41 -0800

    Update run.sh

--- sin/run.sh
+++ sin/run.sh
@@ -5,5 +5,12 @@
 echo "starting on all the ports"
 echo $SSH_VAR
 echo $RAILS_ENV
+
+echo "checking phantomjs"
+phantomjs --version
+
+echo "checking casperjs"
+casperjs --version
+
 bundle exec puma #-p 4000 & bundle exec puma -p 5000
 echo "done started"
