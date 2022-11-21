b1cbf44c0958b3897023feb8de462f333a8bba3b
matt maddex <mmaddex@render.com>
2022-11-21 11:15:58 -0800

    Update casperjs_env.sh

--- sin/casperjs_env.sh
+++ sin/casperjs_env.sh
@@ -6,3 +6,7 @@ export OPENSSL_CONF=/etc/ssl
 # Just need to remove the /sin from the path and this should work for anyone
 export PATH="$PATH:/opt/render/project/src/sin/node_modules/phantomjs-prebuilt/lib/phantom/bin/"
 export PATH="$PATH:/opt/render/project/src/sin/node_modules/casperjs/bin/"
+
+echo "from casperjs_env.sh"
+echo $OPENSSL_CONF
+echo $PATH
