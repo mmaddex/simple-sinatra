486257fc312a013d45a50d091844fd36a29bf9f2
matt maddex <mmaddex@render.com>
2022-11-21 12:40:24 -0800

    Update build.sh

--- sin/build.sh
+++ sin/build.sh
@@ -4,14 +4,10 @@
 # https://www.npmjs.com/package/casperjs
 npm install phantomjs-prebuilt casperjs
 
-pwd
-ls
-echo "bash the env"
-bash ./casperjs_env.sh
+
+./casperjs_env.sh
 echo "from build.sh"
 source ~/.bashrc
-echo $OPENSSL_CONF
-echo $PATH
 casperjs --version
 phantomjs --version
 
