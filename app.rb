665b2f6bb49d5ca41338622f55ea32f37b52097c
matt maddex <mmaddex@render.com>
2023-03-02 11:23:32 -0700

    Update app.rb

--- app.rb
+++ app.rb
@@ -26,7 +26,7 @@ get '/writefile' do
 end
 
 get '/health' do
-  puts "Health check reqeuest from - #{ENV['RENDER_INSTANCE_ID']} - #{request.ip}"
+  puts "Health check reqeuest from - #{ENV['RENDER_INSTANCE_ID']} - #{request.url}"
   status 200
   "orl korrect"
 end
