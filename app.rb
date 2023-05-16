c329af72e01382ed2fa0f676c4ed37168279b196
matt maddex <mmaddex@render.com>
2023-05-16 09:40:20 -0600

    Update app.rb

--- app.rb
+++ app.rb
@@ -23,6 +23,20 @@ get '/secretfile' do
   data_hash.to_s
 end
 
+get '/rootsecretfile' do
+  puts "secretfile"
+  file = File.read('secret.json')
+  data_hash = JSON.parse(file)
+  data_hash.to_s
+end
+
+get '/dotrootsecretfile' do
+  puts "secretfile"
+  file = File.read('./secret.json')
+  data_hash = JSON.parse(file)
+  data_hash.to_s
+end
+
 get '/writefile' do
   File.write('/opt/render/project/test.it', 'Some glorious content', mode: 'a+')
   readit = File.read('/opt/render/project/test.it')
