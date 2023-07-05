c346e2e39973e1879664829f575363d1e0580160
matt maddex <mmaddex@render.com>
2023-07-05 13:43:47 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
-touch ~/.irbrc_run && echo "IRB.conf[:USE_AUTOCOMPLETE] = false" >> ~/.irbrc_run
-cat ~/.irbrc_run
+#touch ~/.irbrc_run && echo "IRB.conf[:USE_AUTOCOMPLETE] = false" >> ~/.irbrc_run
+#cat ~/.irbrc_run
+
 #./casperjs_env.sh
 #source ~/.bashrc
 
 echo "starting on all the ports"
 echo $SSH_VAR
 echo $RAILS_ENV
-echo 'testmd'
-cat test.md
+#echo 'testmd'
+#cat test.md
 
 #echo "checking phantomjs"
 #phantomjs --version
 
 #echo "checking casperjs"
 #casperjs --version
-echo 'secrets.json'
-cat /etc/secrets/secret.json
-echo '.env'
-cat .env
+#echo 'secrets.json'
+#cat /etc/secrets/secret.json
+#echo '.env'
+#cat .env
 
 # set -x
 # bundle exec puma -p 5555 & bundle exec puma -p 5557
 bundle exec puma
-echo "done started"
-ps -ef | grep puma
+#echo "done started"
+#ps -ef | grep puma
 
-touch ~/.irbrc_run && echo "IRB.conf[:USE_AUTOCOMPLETE] = false" >> ~/.irbrc_run
+#touch ~/.irbrc_run && echo "IRB.conf[:USE_AUTOCOMPLETE] = false" >> ~/.irbrc_run
