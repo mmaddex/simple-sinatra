5b5280a5e8d5adf164c5850dfacbb6b982691d7e
mmaddex <mmaddex@render.com>
2024-04-19 15:36:24 -0700

    big change

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ get '/' do
   STDERR.puts "errrrrr"
   STDOUT.puts "ooooot"
   STDOUT.puts "from / - #{ENV['RENDER_INSTANCE_ID']}"
-  "Tudo bem?!??!!!?"
+  "Tudo bem?!??!!?"
 end
 
 get '/header/:val' do
