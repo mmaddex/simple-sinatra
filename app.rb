1619f464c7c415d327d22dc13906a513be022bcb
matt maddex <mmaddex@render.com>
2025-01-15 17:20:18 -0700

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,6 +1,20 @@
 require 'sinatra'
 require 'json'
 require 'puma'
+# Set up a trap for SIGTERM
+Signal.trap("TERM") do
+  puts "Received SIGTERM, shutting down gracefully..."
+  # Perform any cleanup here, e.g., closing database connections
+  sleep 5 # Simulate cleanup time
+  exit 0
+end
+
+# Set up a trap for SIGINT (e.g., Ctrl+C)
+Signal.trap("INT") do
+  puts "Received SIGINT, shutting down gracefully..."
+  # Perform any cleanup here
+  exit 0
+end
 
 get '/' do
   STDERR.puts "errrrrr"
