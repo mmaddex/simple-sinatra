a14915a91e4fbbaced847699ea4847bf69a7b656
matt maddex <mmaddex@render.com>
2025-01-15 18:42:07 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 
 # Trap SIGTERM and SIGINT and define cleanup actions
-trap 'echo "DOINGWHATEVERIWANT"; kill -TERM "$child" 2>/dev/null' SIGTERM SIGINT
+
 
 echo "starting rackup"
 
 # Start Puma as a background process
-bundle exec puma &
-
-# Capture the child's PID
-child=$!
-
-# Wait for the child process to exit
-wait "$child"
+bundle exec puma
