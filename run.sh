7fa09a2746d06952cf7c07c02fec68d29a1244f4
matt maddex <mmaddex@render.com>
2025-01-15 17:51:49 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,39 +1,4 @@
 #!/bin/bash
 
-#touch ~/.irbrc_run && echo "IRB.conf[:USE_AUTOCOMPLETE] = false" >> ~/.irbrc_run
-#cat ~/.irbrc_run
-
-#./casperjs_env.sh
-#source ~/.bashrc
-
-echo "starting on all the ports"
-echo $RENDER_INSTANCE_ID
-echo $RENDER_SERVICE_ID
-echo $SSH_VAR
-echo $RAILS_ENV
-
-#trap 'echo DOINGWHATEVERIWANT' SIGTERM SIGINT
-
-#echo 'testmd'
-#cat test.md
-
-#echo "checking phantomjs"
-#phantomjs --version
-
-#echo "checking casperjs"
-#casperjs --version
-#echo 'secrets.json'
-#cat /etc/secrets/secret.json
-#echo '.env'
-#cat .env
-
-# set -x
-# bundle exec puma
 echo "starting rackup"
 bundle exec rackup
-#& bundle exec puma -p 5557
-#bundle exec puma
-#echo "done started"
-#ps -ef | grep puma
-
-#touch ~/.irbrc_run && echo "IRB.conf[:USE_AUTOCOMPLETE] = false" >> ~/.irbrc_run
