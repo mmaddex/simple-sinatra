df44c64061b8e610a6573163f266f0d5c7deabb7
mmaddex <mmaddex@render.com>
2025-03-06 13:01:45 -0700

    two changes

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ get '/' do
   STDERR.puts "errrrrr"
   STDOUT.puts "ooooot"
   STDOUT.puts "from / - #{ENV['RENDER_INSTANCE_ID']}"
-  "Tudo bem?!?!!?!!!??!@!@!!!!@!!!aa!?\n\n"
+  "Tudo bem?!?!!?!!!??!@!@!!!!@!!!aa!1?\n\n"
 end
 
 post '/test' do
