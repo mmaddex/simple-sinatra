3f9f8bea74ee6320474cf73900276ee60051a671
matt maddex <mmaddex@render.com>
2025-05-06 15:21:01 -0700

    Update app.rb

--- app.rb
+++ app.rb
@@ -22,7 +22,7 @@ get '/' do
   STDERR.puts "errrrrr"
   STDOUT.puts "ooooot"
   STDOUT.puts "from / - #{ENV['RENDER_INSTANCE_ID']}"
-  "Tudo bem?!?!!?!!!??!@!@!!!!@!!!aa!1?\n\n"
+  "Tudo bem?!?!!?!!!??!@!@!!!!@!!!aa!1?\n\n#{Time.now.to_i}"
 end
 
 post '/test' do
