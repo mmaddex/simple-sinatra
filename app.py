0ead0d7941c7f16463924c9a4be83008bcbc21ef
matt maddex <mmaddex@render.com>
2025-10-30 16:50:40 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ import time
 from datetime import datetime
 
 while True:
-    print(f"Current time: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}", flush=True)
+    print(f"Current time: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}", flush=True)
     time.sleep(30)
