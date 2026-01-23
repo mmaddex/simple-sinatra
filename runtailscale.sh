58d972e8ba5ec8860fc22bfc8f2d8711fcaf8929
matt maddex <mmaddex@render.com>
2026-01-23 15:06:06 -0700

    Create runtailscale.sh

---
+++ runtailscale.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+/src/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 & /src/tailscale up --authkey=${TS_AUTHKEY} --hostname=side-car
+/src/tailscale serve --bg 80
+echo Tailscale started
+
+#ALL_PROXY=socks5://localhost:1055/ HTTP_PROXY=http://localhost:1055/ http_proxy=http://localhost:1055/ rackup -o 0.0.0.0
+ALL_PROXY=socks5://localhost:1055/ rackup -o 0.0.0.0 -p 80
+#ALL_PROXY=socks5://localhost:1055/ bundle exec puma -o 0.0.0.0 -p 80
+#rackup -o 0.0.0.0
